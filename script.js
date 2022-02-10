59be154c84f53db98bd0a4a43841afdb11b09291
Louka3 <lkuczykowski@gmail.com>
2022-02-10 00:19:40 -0500

    Added function to create a new grid but running into an issue with keeping the same size grid with certain numbers. Need to look into CSS Grid and using fractions for my square lengths

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/* Need a loop to create 16 square divs to make my Etch-A-Sketch Grid */
+//Only working with certain numbers. Need to look into CSS Grid and fr1 (fractions) for
+//the square lengths. Seems to work better for people than what I'm trying to do
 
 const container = document.querySelector("#container")
 const gridContainer = document.querySelector("#grid-container")
@@ -6,9 +7,9 @@ const clearButton = document.querySelector("#clear-button")
 const gridButton = document.querySelector('#new-grid-count')
 
 clearButton.addEventListener('click',clearBoard)
-gridButton.addEventListener('click', newGrid.bind(this,"?"))
+//gridButton.addEventListener('click', newGrid.bind(this,prompt("How big do you want the grid(side length)?S")))
 
-
+/* Need a loop to create 16 square divs to make my Etch-A-Sketch Grid */
 for (let i = 0;i < 256; i++){
     // for (let i = 0; i < 256; i++){
     let square = document.createElement('div');
@@ -31,7 +32,11 @@ function clearBoard(){
 }
 
 function newGrid(num){
-    //num is the number of squares per side. Default at start of program is 4
-
-
+    //num is the number of squares per side. Default at start of program is 16
+    board = document.querySelectorAll('.grid-square')
+    //square root (921,600 / num^2)
+    let newGridLength = Math.sqrt(921600 / Math.pow(num, 2))
+    for (let b = 0; b < board.length; b++){
+        board[b].setAttribute('style', 'height: 10px; width: 10px')
+    }
 }
