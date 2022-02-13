5a8a549f94094117c3a23f2a5fc08ef36af475b8
Louka3 <lkuczykowski@gmail.com>
2022-02-13 00:27:31 -0500

    Fix makeGrid function to make correct size squares in gridContainer

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-//Only working with certain numbers. Need to look into CSS Grid and fr1 (fractions) for
-//the square lengths. Seems to work better for people than what I'm trying to do
 
 const container = document.querySelector("#container")
-const gridContainer = document.querySelector("#grid-container")
+const gridContainer = document.getElementById('grid-container')
 const clearButton = document.querySelector("#clear-button")
 const gridButton = document.querySelector('#new-grid-count')
-let currentLength = 256 //initiallized for a 16x16 square
 
 
 clearButton.addEventListener('click',clearBoard)
 gridButton.addEventListener('click', newGrid)
 
-/* Need a loop to create 16 square divs to make my Etch-A-Sketch Grid */
-makeGrid(currentLength)
+makeGrid(256)
 
 function makeGrid(squares){
+    let squaresPerSide = (Math.sqrt(squares));
+
+    gridContainer.style.gridTemplateColumns = 'repeat($sps,1fr)'.replace('$sps', squaresPerSide)
+    gridContainer.style.gridTemplateRows = 'repeat($sps,1fr)'.replace('$sps', squaresPerSide)
+  
     for (let i = 0;i < squares; i++){
         let square = document.createElement('div');
         square.className = "grid-square"
         square.addEventListener('mouseover', changeColor.bind(this, square))
         gridContainer.append(square)
+    }
 }
-}
+
 function changeColor(square){
     square.setAttribute('style', 'background-color: orange')
 }
@@ -36,20 +38,7 @@ function clearBoard(){
 }
 
 function newGrid(){
-    //num is the number of squares per side. Default at start of program is 16
-    squares = document.querySelectorAll('.grid-square')
-
-    for (let a = 0; a < squares.length; a++){
-        //gridContainer
-    }
+    document.querySelectorAll('.grid-square').forEach(sq => sq.remove());
     let num = prompt("How big do you want the grid(side length)?")
-    makeGrid((num * num))
-    
-    //square root (921,600 / num^2)
-    let newGridLength = Math.sqrt(921600 / Math.pow(num, 2)).toFixed(1)
-    for (let b = 0; b < squares.length; b++){
-        squares[b].setAttribute('style', 'grid-template-columns: repreat(' + 
-            newGridLength.toString() + ', 1fr); grid-template-rows: repeat(' + 
-            newGridLength.toString() + ', 1fr);')
-    }
+    makeGrid((num * num))    
 }
